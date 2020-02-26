2e3e44c3cda6acd7ab5a8fb4d24913715d34d6e4
fhdali334 <fhdali334@gmail.com>
2020-02-26 17:02:21 +0500

    This is my testing git hub code

---
+++ test.py
@@ -0,0 +1,10 @@
+language = "programming fundamentals"
+print (len(language))
+print (language[0])
+print (language[2])
+print (language[3])
+print (language[4])
+print (language[9])
+print (language[13])
+print (language[0:])
+print (language[:24])
